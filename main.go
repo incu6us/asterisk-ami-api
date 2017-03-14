0c6b0dc35d905f04b3c9241e4c513062ec600810
Vyacheslav Pryimak <incu6us@yandex.ru>
2017-03-14 19:06:46 +0200

    Origination finished

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/bit4bit/gami"
 	"github.com/op/go-logging"
 
 	"github.com/incu6us/asterisk-ami-api/internal/utils/config"
 
-	"github.com/incu6us/asterisk-ami-api/internal/platform/ami"
-	"strconv"
+	"net/http"
+	"github.com/incu6us/asterisk-ami-api/internal/platform/api"
 )
 
 func main() {
@@ -15,27 +14,34 @@ func main() {
 	var log = logging.MustGetLogger("main")
 	var conf = config.GetConfig()
 
-	var amiClient ami.AMI
-	var rsPing <-chan *gami.AMIResponse
+	//var amiClient ami.AMI
+	//var rsPing <-chan *gami.AMIResponse
 	var err error
+	//
+	//var host = conf.Ami.Host + ":" + strconv.Itoa(conf.Ami.Port)
+	//
+	//amiClient = ami.GetAMI(host, conf.Ami.Username, conf.Ami.Password)
+	//if err = amiClient.Run(); err != nil {
+	//	log.Error("Error:", err)
+	//}
 
-	var host = conf.Ami.Host + ":" + strconv.Itoa(conf.Ami.Port)
+	//if rsPing, err = amiClient.Action("Ping", nil); err != nil {
+	//	log.Fatal(err)
+	//} else {
+	//	log.Debug("ping: %v", <-rsPing)
+	//}
+	//
+	//
+	//if rsPing, err = amiClient.Action("Ping", nil); err != nil {
+	//	log.Fatal(err)
+	//} else {
+	//	log.Debug("ping: %v", <-rsPing)
+	//}
 
-	amiClient = ami.GetAMI(host, conf.Ami.Username, conf.Ami.Password)
-	if err = amiClient.Run(); err != nil {
-		log.Error("Error:", err)
-	}
 
-	if rsPing, err = amiClient.Action("Ping", nil); err != nil {
+
+
+	if err = http.ListenAndServe(conf.General.Listen, api.NewHandler()); err != nil {
 		log.Fatal(err)
-	} else {
-		log.Debug("ping: %v", <-rsPing)
-	}
-
-
-	if rsPing, err = amiClient.Action("Ping", nil); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Debug("ping: %v", <-rsPing)
 	}
 }
