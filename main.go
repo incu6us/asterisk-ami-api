7d7af0497898d7569f3b03f286283744d36c9d53
Vyacheslav Pryimak <incu6us@yandex.ru>
2017-03-15 15:27:39 +0200

    README added

--- main.go
+++ main.go
@@ -14,32 +14,7 @@ func main() {
 	var log = logging.MustGetLogger("main")
 	var conf = config.GetConfig()
 
-	//var amiClient ami.AMI
-	//var rsPing <-chan *gami.AMIResponse
 	var err error
-	//
-	//var host = conf.Ami.Host + ":" + strconv.Itoa(conf.Ami.Port)
-	//
-	//amiClient = ami.GetAMI(host, conf.Ami.Username, conf.Ami.Password)
-	//if err = amiClient.Run(); err != nil {
-	//	log.Error("Error:", err)
-	//}
-
-	//if rsPing, err = amiClient.Action("Ping", nil); err != nil {
-	//	log.Fatal(err)
-	//} else {
-	//	log.Debug("ping: %v", <-rsPing)
-	//}
-	//
-	//
-	//if rsPing, err = amiClient.Action("Ping", nil); err != nil {
-	//	log.Fatal(err)
-	//} else {
-	//	log.Debug("ping: %v", <-rsPing)
-	//}
-
-
-
 
 	if err = http.ListenAndServe(conf.General.Listen, api.NewHandler()); err != nil {
 		log.Fatal(err)
